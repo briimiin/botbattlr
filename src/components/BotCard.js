2075218a290b2b1b2babd4778329c63e8aaec01b
briimiin <kipkorirbrimin005@gmail.com>
2024-05-06 15:42:01 +0300

    styles for bot card

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -20,39 +20,24 @@ const BotCard = (props) => {
     };
 
   return (
-    <div className="bot-card-grid">
       <div className="ui card small-card" >
-      
-        <div className="image">
           <img alt="bot" src={bot.avatar_url} />
-        </div>
-        <div className="content">
-          <div>
-            Name: {bot.name}
-            <i className={botTypeClasses[props.bot.bot_class]}></i>
-          </div>
-          <div>
-            <small>{bot.catchphrase}</small>
-          </div>
-        </div>
-        <div className="extra-content">
+          Name: {bot.name}
+          <i className={botTypeClasses[props.bot.bot_class]}></i>
+          <br></br>
+          <small>{bot.catchphrase}</small>
+          <br></br>
           <span>
             Health: {bot.health} | Damage: {bot.damage} | Armor: {bot.armor}|
           </span>
-          <div>
-            <div>
-              Class: {bot.bot_class}
-            </div>
-            <button className="button" onClick={handleDischargeForever}>
-              x
-            </button>
-          </div>
-        </div>
-        <div> 
+          <br></br>
+          Class: {bot.bot_class}
+          <br></br>
+          <button className="button" onClick={handleDischargeForever}> x </button>
+          <br></br>
           <button onClick={handleClick}>Add to army</button>
-        </div>
+       
       </div>
-    </div>
   );
 };
 
